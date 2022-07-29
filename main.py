67e21849ac5aa97b7c3476796216d30baeb9f686
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-29 20:50:48 +0530

    Concepts of import, print and variables

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+import math
+
+
+print("hello")
+print(math.gcd(3, 6))
+
+# comment
+''' 
+multiline comment
+
+
+age=int(input())
+if(age<18):
+    print("child")
+elif(age>18 and age<21):
+    print("into adulthood")
+else:
+    print("Adult")
+
+'''
+
+'''
+VARIABLES
+start with a letter or underscore
+not start with a number
+only alpha-numeric characters
+names are case-sensitive
+'''
+
+'''
+a = 27  # integer
+b = "rishabh"  # string
+c = 3.14  # float
+d = 2
+'''

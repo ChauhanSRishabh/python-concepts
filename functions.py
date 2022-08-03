634e18c1d4173638a3612027cbd28524ccb9d8e2
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-08-03 14:02:47 +0530

    Define and Call Functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+# FUNCTIONS
+
+# Syntax
+def greet():
+    print("Namaste")
+    print("kya haal chaal")
+    print("kya khaoge")
+
+greet()
+
+# With arguments
+def sum(a,b):
+    return a+b
+
+print(sum(2,3))
+
